7ffc752f95c8481a39e02108d6a345f646d42efb
tznakhtr <tazeenakhtar032@gmail.com>
2026-06-01 02:06:06 +0530

    add root route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
   return (
      <BrowserRouter>
      <Routes>
+      <Route path= "/" element={<Login/>}/>
       <Route path ='/register' element ={<Signup/>}/>
       <Route path ='/Login' element ={<Login/>}/>
       <Route path ='/home' element ={<Home/>}/>
